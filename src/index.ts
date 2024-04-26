b6280e5f5cdaec135415d5ef5acda06aedcaf5b3
Deepak <ideepakrabindra@gmail.com>
2024-04-26 18:49:51 +0530

    - feat: Add .env.example and nodemon.json - chore: Update package-lock.json and package.json - fix: Update src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
+import { config } from 'dotenv-safe';
 import add from 'math/add';
 
+config();
+
 console.log('Hello');
-console.log(add(1, 2));
+console.log(add(3, 5));
+
+console.log(process.env.MY_NAME);
